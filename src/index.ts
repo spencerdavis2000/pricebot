d704ba82170b485daef584b7665f5666002d459f
Spencer Davis <spencer.w.davis@accenture.com>
2021-09-30 23:12:43 -0500

    added dictionary price holder as well as hasThreshold function and logic to check, logs and also chalk to colorize the logs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 import { tickers } from './interfaces/tickers';
+import chalk from 'chalk';
 export class UpholdBot {
   public async getTickers(): Promise<unknown> {
     return fetch('https://api.uphold.com/v0/ticker/USD')
@@ -22,18 +23,55 @@ export class UpholdBot {
   }
 }
 
+function hasThreshold(
+  baseprice: number,
+  currentprice: number,
+  threshold: number
+): boolean {
+  const upThreshold = 1 + threshold;
+  const downThreshold = 1 - threshold;
+  if (
+    currentprice > baseprice * upThreshold ||
+    currentprice < baseprice * downThreshold
+  ) {
+    return true;
+  }
+  return false;
+}
+// dictionary map holder for base prices
+const baseprices: { [pair: string]: number } = {};
+
 async function PriceBotOscillator(
   currencies: string[],
   upholdBot: UpholdBot
 ): Promise<void> {
-  await upholdBot.getTickers();
+  const tickers = await upholdBot.getTickers();
   const myTickers = await upholdBot.getCurrencyPairs(currencies);
   myTickers.map((ticker) => {
-    console.log(ticker);
+    // if it does exist find out threshold
+    if (Object.keys(baseprices).length > 0) {
+      if (hasThreshold(baseprices[ticker.pair], parseFloat(ticker.bid), 0.0001)) {
+        // log out the threshold message
+        console.log(
+          chalk.bold.red(
+            `Threshold hit on currency: ${ticker.pair} | at price: ${ticker.bid}`
+          )
+        );
+        // set a new staring price
+        baseprices[ticker.pair] = parseFloat(ticker.bid);
+      }
+    } else {
+      baseprices[ticker.pair] = parseFloat(ticker.bid);
+    }
+    // general log of prices to see it working
+    console.log(chalk.cyan('Threshold is 0.01 percent so 0.0001 fraction'));
+    console.log(chalk.yellow('COMPARING BID: first price | '), baseprices);
+    console.log(chalk.green('COMPARING BID: current price | '), ticker);
   });
 }
 
 let interval = 0;
+
 function start(currencies: string[]) {
   const upholdBot = new UpholdBot();
   global.setTimeout(() => {
